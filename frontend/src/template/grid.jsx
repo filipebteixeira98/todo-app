bed606a5d3376f4e855d55d10719c6cea049bba2
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2025-06-13 21:43:56 -0300

    fix(grid): component to apply classes for responsiveness

--- frontend/src/template/grid.jsx
+++ frontend/src/template/grid.jsx
@@ -1,25 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Grid extends Component {
   toCssClasses(numbers) {
-    const cols = numbers ? numbers.split(' ') : []
-    const devices = ['xs', 'sm', 'md', 'lg']
-    let classes = ''
+    const cols = numbers ? numbers.split(" ") : [];
 
-    cols ? cols.forEach((item, index) => {
-      cols[index] ? classes + `col-${devices[index]}-${cols[index]}` : ''
-    }) : null
+    const devices = ["xs", "sm", "md", "lg"];
 
-    return classes
+    let classes = "";
+
+    cols
+      ? cols.forEach((item, index) => {
+          if (item) classes += `col-${devices[index]}-${item}`;
+        })
+      : null;
+
+    return classes;
   }
 
   render() {
-    const gridClasses = this.toCssClasses(this.props.cols || 12)
+    const gridClasses = this.toCssClasses(this.props.cols || 12);
 
-    return (
-      <div className={gridClasses}>
-        {this.props.children}
-      </div>
-    )
+    return <div className={gridClasses}>{this.props.children}</div>;
   }
 }
